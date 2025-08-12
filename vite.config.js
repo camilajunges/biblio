95285b9fb5e213e17e8d6fa7833eb9b2454e19f2
camilajunges <camilasjunges@gmail.com>
2025-08-12 12:00:23 -0300

    🎨 Refactor project structure

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import { resolve } from "path"
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": resolve(__dirname, "src"),
+      "@/components": resolve(__dirname, "src/components"),
+      "@/assets": resolve(__dirname, "src/assets"),
+      "@/utils": resolve(__dirname, "src/utils"),
     },
+  },
+  server: {
+    port: 3000,
+    open: true,
   },
 })
